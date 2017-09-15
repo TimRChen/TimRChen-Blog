a787c37b16ade2be24f59da05b8d9fbefbdcf8d5
Tim <timrchen@foxmail.com>
2017-09-15 13:35:21 +0800

    fix: 修改测试环境与线上环境domain

--- src/apis/essayAPIs.js
+++ src/apis/essayAPIs.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
+import domains from './domains';
+const addDomain = domain => path => domain + path;
+const { timDomain } = domains;
 
 const APIs = {
-  POST_NEW_ESSAY: 'http://127.0.0.1:3000/api/essay/new', // 新建文章接口
-  GET_ESSAY_LIST: 'http://127.0.0.1:3000/api/essay/list', // 获取文章列表
-  GET_ESSAY_DETAILS: 'http://127.0.0.1:3000/api/essay/details', // 获取文章详细内容
+  POST_NEW_ESSAY: '/api/essay/new', // 新建文章接口
+  GET_ESSAY_LIST: '/api/essay/list', // 获取文章列表
+  GET_ESSAY_DETAILS: '/api/essay/details', // 获取文章详细内容
 };
 
-export default APIs;
+
+export default _.each(APIs, (path, key) => {
+  APIs[key] = addDomain(timDomain)(path);
+});
