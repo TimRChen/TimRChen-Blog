8eec6110518bd5a64f636ecc3a92794911190111
Tim <timrchen@foxmail.com>
2017-10-30 15:44:11 +0800

    更新footer样式

--- src/components/TimFooter.vue
+++ src/components/TimFooter.vue
@@ -1,9 +1,12 @@
 <template>
-  <footer class="site-footer clearfix u-textAlignCenter">
-    <p class="copyright">Copyright © 2017 TimRChen - Just relax in here</p>
-    <a href="http://www.miitbeian.gov.cn/" target="_blank">
-      <span class="tag is-black">京ICP备17058358号-1</span>
-    </a>
+  <footer class="site-footer">
+    <p class="copyright">Copyright © 2017 TimRChen</p>
+    <div class="link-group">
+      <span class="tag is-black">
+        <a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备17058358号-1</a>
+      </span>
+      <span class="tag is-black">友情链接：<a href="https://m.kuaidi100.com/" target="_blank">快递查询</a></span>
+    </div>
   </footer>
 </template>
 
@@ -16,26 +19,27 @@
 <style>
 /* footer.pug */
   .site-footer {
-    padding: 30px 0 30px 0;
+    padding: 30px 0 0 0;
     font-family: exoregular;
-    color: rgba(0,0,0,.44);
-  }
-  .u-textAlignCenter {
     text-align: center;
-  }
-
-  .copyright {
-    margin-bottom: 10px;
+    color: rgba(0,0,0,.44);
   }
 
   /* 竖屏 */
   @media screen and (orientation:portrait) and (max-width: 720px) {
     .site-footer {
       font-size: 14px;
-      padding: 30px 0 30px 0;
+      padding: 30px 0 0 0;
       font-family: exoregular;
+      text-align: center;
       color: rgba(0,0,0,.44);
     }
+  }
+
+  .link-group {
+    width: 100%;
+    background-color: #000;
+    margin-top: 30px;
   }
 
 </style>
