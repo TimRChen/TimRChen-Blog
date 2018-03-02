4535ac30e3ed70db05ec16ed47dfb53b3a591d5f
Tim <timrchen@foxmail.com>
2018-03-02 11:12:36 +0800

    adjust: Extra menu level

--- src/components/base/Extra.vue
+++ src/components/base/Extra.vue
@@ -12,10 +12,61 @@
 
     <div class="feature-box">
       <div class="card">
-        <!-- 新闻资讯服务 START -->
-        <news></news>
-        <!-- 短链接服务 START -->
-        <short-url></short-url>
+        <!-- 1 -->
+        <a v-on:click="opened = 'news'">
+          <div class="card" v-show="opened === 'normal'">
+            <div class="card-content">
+              <div class="media">
+                <h1 class="title is-5">军事新闻资讯</h1>
+              </div>
+            </div>
+          </div>
+        </a>
+        <!-- 2 -->
+        <a v-on:click="opened = 'shortUrl'">
+          <div class="card" v-show="opened === 'normal'">
+            <div class="card-content">
+              <div class="media">
+                <h1 class="title is-5">短链接转换</h1>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a class="back-to-main" v-on:click="opened = 'normal'">
+          <div class="card" v-show="opened !== 'normal'">
+            <div class="card-content">
+              <div class="media">
+                <h1 class="title is-5">点击此处进行返回</h1>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <transition name="component-jump" mode="out-in">
+          <!-- 新闻资讯服务 START -->
+          <div class="news" v-show="opened === 'news'">
+            <news></news>
+          </div>
+        </transition>
+
+        <transition name="component-jump" mode="out-in">
+          <!-- 短链接服务 START -->
+          <div class="short-url" v-show="opened === 'shortUrl'">
+            <short-url></short-url>
+          </div>
+        </transition>
+
+        <a class="back-to-main" v-on:click="opened = 'normal'">
+          <div class="card" v-show="opened !== 'normal' && opened !== 'shortUrl'">
+            <div class="card-content">
+              <div class="media">
+                <h1 class="title is-5">点击此处进行返回</h1>
+              </div>
+            </div>
+          </div>
+        </a>
+
       </div>
     </div>
 
@@ -29,6 +80,11 @@
   import ShortUrl from '@/components/base/shortUrl/ShortUrl';
 
   export default {
+    data() {
+      return {
+        opened: 'normal' // 打开状态
+      }
+    },
     components: {
       News,
       ShortUrl
@@ -49,6 +105,16 @@
 
   .feature-box {
     margin-top: 10px;
+  }
+
+  /* Vue 多组件切换过渡动画 */
+  .component-jump-enter-active, .component-jump-leave-active {
+    transition: transform .5s ease-in;
+  }
+  .component-jump-enter, .component-jump-leave-to
+  /* .component-jump-leave-active for below version 2.1.8 */ {
+    /* opacity: 0; */
+    transform: scale(2, 2)
   }
 
 
